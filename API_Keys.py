392634e477ebfc171678325bd419c37472e1ab28
Davis Keene <dbkeene.tsyc@gmail.com>
2018-11-24 12:51:09 -0500

    removed api keys

--- API_Keys.py
+++
@@ -1,3 +0,0 @@
-# slack_token  = "xoxb-315591740165-nRKQC9lZhy5FLWltkQcbWU8b"
-slack_token = "xoxb-10024790197-431317607797-GqIAgeRwpPYHzLyr2jL9uxn2"
-heroku_key = '119be864-9d74-4722-803f-4e05a06da242'
